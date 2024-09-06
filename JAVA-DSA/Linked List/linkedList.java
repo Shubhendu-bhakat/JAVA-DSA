50608131cfbd7e8f21f5434ac0d5429e9c371856
Shubhendu Bhakat <kumarsubho2003@gmail.com>
2024-09-06 20:48:54 +0530

    ll

--- JAVA-DSA/Linked List/linkedList.java
+++ JAVA-DSA/Linked List/linkedList.java
@@ -1,3 +1,5 @@
+import java.security.PublicKey;
+
 public class linkedList {
     public static class Node {
         int data;
@@ -11,10 +13,12 @@ public class linkedList {
 
     public static Node head;
     public static Node Tail;
+    public static int size;
 
     public void addFirst(int data) {
         // step 1 = create new node
         Node newNode = new Node(data);
+        size++;
         // check wether the linkedlist is empty or not
         if (head == null) {
             head = Tail = newNode;
@@ -29,6 +33,7 @@ public class linkedList {
     public void addLast(int data) {
         // s1 create a new node
         Node newNode = new Node(data);
+        size++;
         // check wether the ll is empty or not
         if (head == null) {
             head = Tail = newNode;
@@ -54,13 +59,13 @@ public class linkedList {
     }
 
     public void addAnywhere(int index, int data) {
-        // s1 create the new node
-        Node newNode = new Node(data);
-        // check wether LL is empty
-        if (head == null) {
-            head = Tail = newNode;
+        if(index == 0){
+            addFirst(data);
             return;
         }
+        // s1 create the new node
+        Node newNode = new Node(data);
+        size++;
         //create a temp variable for to traverse ll of Node type
         Node temp = head;
         int idx = 0; // tracking variable for ll
@@ -71,20 +76,123 @@ public class linkedList {
         newNode.next = temp.next;  //newNode.next = points to the Node that is temp is pointing previously
         temp.next = newNode; //temp will now point to newNode and the linkage is made and broken with previous
     }
+        public int removeFirst(){
+            if(size ==0){
+                System.out.println("UnderFlow");
+                return Integer.MIN_VALUE;
+            }else if(size ==1){
+                int val = head.data;
+                head = Tail = null;
+                return val;
+            }
+            int val = head.data;
+            head = head.next;
+            size--;
+            return val;
+        }
+    public int removeLast(){
+       if(size == 0){
+        System.out.println("UnderFlow");
+        return Integer.MIN_VALUE;
+       }else if(size == 1){
+        int val = head.data;
+        head = Tail = null;
+        size--;
+        return val;
+       }
+       Node temp = head;
+       for(int i=0;i<size-2;i++){
+        temp = temp.next;//increment of temp untill it reaches to the Tail-2 node 
+       }
+       int val = temp.next.data;
+       temp.next = null;
+       Tail = temp;
+       size--;
+       return val;
+    }
+    public int searchLL(int data){
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == data){
+                return temp.data;
+            }
+            temp = temp.next;
+        }
+        return -1;
+    }
+    public static int helper(Node head,int key){
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx+1;
+    }
+
+
+    public static int RecursiveSearch(int key){
+        return helper(head, key);
+    }
+
+    public  void Reverse(Node head){
+        Node prev = null;
+        Node curr = head;
+        Node next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    //find and remove from last formula is (size-n+1)
+    public void findAndRemoveFromLast(Node head,int idx){
+        int index = 1;
+        int sz=0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+        System.out.println("size is"+sz);
+        if(idx == sz){
+            head = head.next;
+            return;//remove the head and only 1 elements was in ll
+        }
+        Node prev = head;
+        while(index < sz-idx){ //when you will be out of this loop you will be on the prev node to delete
+            prev = prev.next;
+            index++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
 
     public static void main(String[] args) {
         linkedList LL = new linkedList();
-        LL.printLL();
+      
         LL.addFirst(2);
-        LL.printLL();
+        
         LL.addFirst(1);
-        LL.printLL();
+    
         LL.addLast(3);
-        LL.printLL();
+  
         LL.addLast(4);
+        // LL.addAnywhere(3, 87);
+        // LL.removeLast();
         LL.printLL();
-        LL.addAnywhere(2, 87);
-        LL.printLL();
+        // System.out.println(LL.searchLL(87));
+        // System.out.println(RecursiveSearch(87));
+    //    LL.Reverse(head);
+     
+       LL.findAndRemoveFromLast(head, 1);
+       LL.printLL();
     }
 
 }
